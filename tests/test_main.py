fd457f3f2a71ae94bcd1af92c4c974431003e84f
34j <55338215+34j@users.noreply.github.com>
2024-10-24 11:29:41 +0900

    feat: add main feat (#4)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,15 @@
-from shift_nth_row_n_steps.main import add
+import ivy
+
+from shift_nth_row_n_steps._main import (
+    shift_nth_row_n_steps,
+    shift_nth_row_n_steps_for_loop,
+)
+
+ivy.set_backend("numpy")
 
 
-def test_add():
-    """Adding two number works as expected."""
-    assert add(1, 1) == 2
+def test_shift_nth_row_n_steps() -> None:
+    input = ivy.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
+    expected = ivy.array([[[1, 2, 3], [0, 4, 5], [0, 0, 7]]])
+    assert ivy.array_equal(shift_nth_row_n_steps(input, cut_padding=True), expected)
+    assert ivy.array_equal(shift_nth_row_n_steps_for_loop(input), expected)
